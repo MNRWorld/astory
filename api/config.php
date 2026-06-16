2363934539bfdd3e93c0a1d61f6d10a7a266d205
vjsdk <ndurjoy121@gmail.com>
2026-06-16 23:04:04 +0600

    Let .env override environment variables and default DB_SSLMODE to disable

--- api/config.php
+++ api/config.php
@@ -37,25 +37,10 @@ if (file_exists($envPath)) {
             $name = trim($name);
             $value = trim($value, " \t\n\r\0\x0B\"'"); // remove quotes and spaces
             
-            // Check if it's already defined in the server environment
-            $serverVal = getenv($name);
-            if ($serverVal === false || $serverVal === '') {
-                if (!isset($_ENV[$name])) {
-                    $_ENV[$name] = $value;
-                }
-                if (!isset($_SERVER[$name])) {
-                    $_SERVER[$name] = $value;
-                }
-                @putenv("$name=$value");
-            } else {
-                // If it is already defined in getenv, make sure $_ENV and $_SERVER also have it
-                if (!isset($_ENV[$name])) {
-                    $_ENV[$name] = $serverVal;
-                }
-                if (!isset($_SERVER[$name])) {
-                    $_SERVER[$name] = $serverVal;
-                }
-            }
+            // .env.local values should take precedence over existing environment variables
+            $_ENV[$name] = $value;
+            $_SERVER[$name] = $value;
+            @putenv("$name=$value");
         }
     }
 }
@@ -66,7 +51,7 @@ define('DB_NAME', getEnvVar('DB_NAME', 'your_database_name'));
 define('DB_USER', getEnvVar('DB_USER', 'your_database_user'));
 define('DB_PASS', getEnvVar('DB_PASS', 'your_database_password'));
 define('DB_PORT', getEnvVar('DB_PORT', '5432'));
-define('DB_SSLMODE', getEnvVar('DB_SSLMODE', 'prefer'));
+define('DB_SSLMODE', getEnvVar('DB_SSLMODE', 'disable'));
 
 // Security Key for JWT (Change this to a long random string)
 define('JWT_SECRET', getEnvVar('JWT_SECRET', 'your-very-secret-key-12345'));
